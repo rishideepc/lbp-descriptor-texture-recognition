21e8a48295d269baeb6b81c6cca65bfe3ce347d2
rishideepc <rishi1a2@gmail.com>
2023-04-07 17:50:24 +0530

    Reformatted comments

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-#   Preprocessing Image - before LBP Feature Extraction
-#   Why?
-#   The first step is to preprocess the image to remove any noise present in it. 
-#   This can be done using a denoising filterS, such as the Gaussian filter or the median filter. 
-#   The denoising filter removes any and all noise, while preserving the texture information.
-#   This enhances the discriminative power of the LBP descriptor, 
-#   and facilitates the Texture Recognition process.
-#   We are using the median filter in this scenario. Usage can vary depending on the type of image!
-
 import cv2
 from skimage.feature import local_binary_pattern
 import numpy as np
@@ -16,9 +7,15 @@ img = cv2.imread('assets/images/image_1.jpeg')              # Reading Image from
 dimensions = (400, 400)
 img = cv2.resize(img, dimensions)
 
-#   Usage of OpenCV (cv2) Python libraries simplifies 
-#   image preprocessing due to presence of 
-#   built-in functions like cvtColor, medianBlur, GaussianBlur, etc.
+
+#   Preprocessing Image - before LBP Feature Extraction
+#   Why?
+#   The first step is to preprocess the image to remove any noise present in it. 
+#   This can be done using a denoising filterS, such as the Gaussian filter or the median filter. 
+#   The denoising filter removes any and all noise, while preserving the texture information.
+#   This enhances the discriminative power of the LBP descriptor, 
+#   and facilitates the Texture Recognition process.
+#   We are using the median filter in this scenario. Usage can vary depending on the type of image!
 
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)                # Method 'cvtColor' is used to convert the given original image into grayscale for further denoising
